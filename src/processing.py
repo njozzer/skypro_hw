01ac73e548435a0d71eae697d8406fff82e95592
Max <maxi4o@mail.ru>
2026-06-05 20:38:24 +0300

    fixes

--- src/processing.py
+++ src/processing.py
@@ -18,15 +18,19 @@ def sort_by_date(list_to_sort: list, descending: bool = True) -> list:
     убывание).
     Функция возвращает отсортированный список по дате.
     """
-    sorted_list = sorted(list_to_sort, key=lambda x: x["date"], reverse=descending)
-    return sorted_list
+
+    return sorted(list_to_sort, key=lambda x: x["date"], reverse=descending)
 
 
 if __name__ == "__main__":
     data = [
-        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+        {'id': 41428829, 'state': 'EXECUTED',
+         'date': '2019-07-03T18:35:29.512364'},
+        {'id': 939719570, 'state': 'EXECUTED',
+         'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELED',
+         'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED',
+         'date': '2018-10-14T08:21:33.419441'}]
     print(filter_by_state(data))
     print(sort_by_date(data))
