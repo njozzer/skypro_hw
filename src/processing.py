dae015c0d4cbf2b8df81ac4c1f6d8a9dceeb5e9e
Max <maxi4o@mail.ru>
2026-06-05 20:37:36 +0300

    fixes

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(dict_list: list, state: str = "EXECUTED") -> list:
+def filter_by_state(list_to_filter: list, state: str = "EXECUTED") -> list:
     """
     Функция filter_by_state, которая принимает список словарей и
     опционально значение для ключа state (по умолчанию
@@ -7,19 +7,19 @@ def filter_by_state(dict_list: list, state: str = "EXECUTED") -> list:
     только те словари, у которых ключ state соответсвует
     значению переменной state.
     """
-    tmp_list = [item for item in dict_list if item["state"] == state]
-    return tmp_list
+    filtered_list = [item for item in list_to_filter if item["state"] == state]
+    return filtered_list
 
 
-def sort_by_date(dict_list: list, descending: bool = True) -> list:
+def sort_by_date(list_to_sort: list, descending: bool = True) -> list:
     """
     Функция sort_by_date, которая принимает список словарей и
     опционально значение, задающее порядок сортировки (по умолчанию
     убывание).
     Функция возвращает отсортированный список по дате.
     """
-    tmp_list = sorted(dict_list, key=lambda x: x["date"], reverse=descending)
-    return tmp_list
+    sorted_list = sorted(list_to_sort, key=lambda x: x["date"], reverse=descending)
+    return sorted_list
 
 
 if __name__ == "__main__":
