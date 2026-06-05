d2ce04b5625dc095e088a5e7595f83f8acec265c
Max <maxi4o@mail.ru>
2026-06-06 00:04:06 +0300

    fixes

--- src/processing.py
+++ src/processing.py
@@ -22,15 +22,4 @@ def sort_by_date(list_to_sort: list, descending: bool = True) -> list:
     return sorted(list_to_sort, key=lambda x: x["date"], reverse=descending)
 
 
-if __name__ == "__main__":
-    data = [
-        {'id': 41428829, 'state': 'EXECUTED',
-         'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED',
-         'date': '2018-06-30T02:08:58.425572'},
-        {'id': 594226727, 'state': 'CANCELED',
-         'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED',
-         'date': '2018-10-14T08:21:33.419441'}]
-    print(filter_by_state(data))
-    print(sort_by_date(data))
+
