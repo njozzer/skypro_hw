7002080c9613414a2bda974dbf099ff7e0f3b2b9
Max <maxi4o@mail.ru>
2026-06-06 10:54:44 +0300

    fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,3 +24,23 @@ def masks_get_mask_card_number_data() -> str:
 @pytest.fixture
 def masks_get_mask_account_data() -> str:
     return "64686473678894779589"
+
+
+@pytest.fixture
+def processing_filter_by_state_data() -> tuple[list, str]:
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ], "EXECUTED"
+
+
+@pytest.fixture
+def processing_sort_by_date_data() -> tuple[list, bool]:
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ], True
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -37,6 +37,13 @@ def test_filter_by_state(list_to_filter: list, state: str, expected: list) -> No
     assert processing.filter_by_state(list_to_filter, state) == expected
 
 
+def test_filter_by_state_fixture(processing_filter_by_state_data: tuple[list, str]) -> None:
+    assert processing.filter_by_state(processing_filter_by_state_data[0], processing_filter_by_state_data[1]) == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+
+
 @pytest.mark.parametrize(
     "list_to_sort, descending, expected",
     [
@@ -61,3 +68,12 @@ def test_filter_by_state(list_to_filter: list, state: str, expected: list) -> No
 )
 def test_sort_by_date(list_to_sort: list, descending: bool, expected: list) -> None:
     assert processing.sort_by_date(list_to_sort, descending) == expected
+
+
+def test_sort_by_date_fixture(processing_sort_by_date_data: tuple[list, bool]) -> None:
+    assert processing.sort_by_date(processing_sort_by_date_data[0], processing_sort_by_date_data[1]) == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
