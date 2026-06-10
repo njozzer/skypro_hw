3c790b650c07c2cc3986eb993fb409bf250b1a80
Max <maxi4o@mail.ru>
2026-06-10 20:59:10 +0300

    fixes

--- src/reader.py
+++ src/reader.py
@@ -10,14 +10,13 @@ def csv_read(file_path: str) -> list[dict]:
     :return: список транзакций
     """
     data_list = []
-    try:
-        with open(file_path, encoding="utf-8") as file:
-            reader = csv.DictReader(file, delimiter=";")
-            for row in reader:
-                data_list.append(row)
-            return data_list
-    except Exception as e:
-        raise e
+    if not isinstance(file_path, str):
+        return data_list
+    with open(file_path, encoding="utf-8") as file:
+        reader = csv.DictReader(file, delimiter=";")
+        for row in reader:
+            data_list.append(row)
+        return data_list
 
 
 def excel_read(file_path: str) -> list[dict]:
@@ -27,14 +26,12 @@ def excel_read(file_path: str) -> list[dict]:
     :return: список транзакций
     """
     data_list = []
-    try:
-        reader = pd.read_excel(file_path)
-        print(reader.shape)
-        for index, row in reader.iterrows():
-            data_list.append(dict(row))
+    if not isinstance(file_path, str):
         return data_list
-    except Exception as e:
-        raise e
+    reader = pd.read_excel(file_path)
+    for index, row in reader.iterrows():
+        data_list.append(dict(row))
+    return data_list
 
 
 if __name__ == "__main__":
--- tests/test_reader.py
+++ tests/test_reader.py
@@ -1,13 +1,76 @@
-import pytest
+from unittest.mock import mock_open, patch
+
+import pandas as pd
 
 from src import reader
 
-from unittest.mock import patch
 
 def test_csv_reader() -> None:
-    with patch("builtins.open") as mock_open:
-        pass
-    pass
+    data = (
+        "id;state;date;amount;currency_name;currency_code;from;to;description"
+        "\n650703;EXECUTED;2023-09-05T11:30:32Z;16210;Sol;PEN;Счет 58803664561298323391;Счет 39745660563456619397;Перевод организации"  # noqa: E501
+    )
+    with patch("builtins.open", mock_open(read_data=data)) as mock_read:
+
+        assert reader.csv_read("data/transactions.csv") == [
+            {
+                "id": "650703",
+                "state": "EXECUTED",
+                "date": "2023-09-05T11:30:32Z",
+                "amount": "16210",
+                "currency_name": "Sol",
+                "currency_code": "PEN",
+                "from": "Счет 58803664561298323391",
+                "to": "Счет 39745660563456619397",
+                "description": "Перевод организации",
+            }
+        ]
+        mock_read.assert_called_with("data/transactions.csv", encoding="utf-8")
+
+
+def test_csv_writer_exception() -> None:
+    data = (
+        "id;state;date;amount;currency_name;currency_code;from;to;description"
+        "\n650703;EXECUTED;2023-09-05T11:30:32Z;16210;Sol;PEN;Счет 58803664561298323391;Счет 39745660563456619397;Перевод организации"
+    # noqa: E501
+    )
+    with patch("builtins.open", mock_open(read_data=data)) as mock_read:
+        assert reader.csv_read(-1) == []    # type: ignore[arg-type]
+
+
 
 def test_excel_reader() -> None:
+
+    with patch("pandas.read_excel") as mock_read:
+        fake_df = pd.DataFrame(
+            {
+                "id": ["650703"],
+                "state": ["EXECUTED"],
+                "date": ["2023-09-05T11:30:32Z"],
+                "amount": ["16210"],
+                "currency_name": ["Sol"],
+                "currency_code": ["PEN"],
+                "from": ["Счет 58803664561298323391"],
+                "to": ["Счет 39745660563456619397"],
+                "description": ["Перевод организации"],
+            }
+        )
+        mock_read.return_value = fake_df
+        assert reader.excel_read("data/transactions.excel") == [
+            {
+                "id": "650703",
+                "state": "EXECUTED",
+                "date": "2023-09-05T11:30:32Z",
+                "amount": "16210",
+                "currency_name": "Sol",
+                "currency_code": "PEN",
+                "from": "Счет 58803664561298323391",
+                "to": "Счет 39745660563456619397",
+                "description": "Перевод организации",
+            }
+        ]
+        mock_read.assert_called_once_with("data/transactions.excel")
+
+
+def test_excel_reader_exception() -> None:
     pass
