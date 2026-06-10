35bfe5b3835efd8aa6a5a51c8d4ed93058ec8620
Max <maxi4o@mail.ru>
2026-06-10 21:01:26 +0300

    final

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -31,12 +31,11 @@ def test_csv_reader() -> None:
 def test_csv_writer_exception() -> None:
     data = (
         "id;state;date;amount;currency_name;currency_code;from;to;description"
-        "\n650703;EXECUTED;2023-09-05T11:30:32Z;16210;Sol;PEN;Счет 58803664561298323391;Счет 39745660563456619397;Перевод организации"
-    # noqa: E501
+        "\n650703;EXECUTED;2023-09-05T11:30:32Z;16210;Sol;PEN;Счет 58803664561298323391;Счет 39745660563456619397;Перевод организации"  # noqa: E501
     )
     with patch("builtins.open", mock_open(read_data=data)) as mock_read:
-        assert reader.csv_read(-1) == []    # type: ignore[arg-type]
-
+        assert reader.csv_read(-1) == []  # type: ignore[arg-type]
+        assert mock_read.call_count == 0
 
 
 def test_excel_reader() -> None:
@@ -73,4 +72,20 @@ def test_excel_reader() -> None:
 
 
 def test_excel_reader_exception() -> None:
-    pass
+    with patch("pandas.read_excel") as mock_read:
+        fake_df = pd.DataFrame(
+            {
+                "id": ["650703"],
+                "state": ["EXECUTED"],
+                "date": ["2023-09-05T11:30:32Z"],
+                "amount": ["16210"],
+                "currency_name": ["Sol"],
+                "currency_code": ["PEN"],
+                "from": ["Счет 58803664561298323391"],
+                "to": ["Счет 39745660563456619397"],
+                "description": ["Перевод организации"],
+            }
+        )
+        mock_read.return_value = fake_df
+        assert reader.excel_read(-1) == []  # type: ignore[arg-type]
+        assert mock_read.call_count == 0
