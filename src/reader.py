469823d2ba72ab4d38c3c4d0e23e2cb8f6866c87
Max <maxi4o@mail.ru>
2026-06-10 12:38:47 +0300

    created files

--- src/reader.py
+++ src/reader.py
@@ -1,13 +1,42 @@
 import csv
+
 import pandas as pd
 
+
 def csv_read(file_path: str) -> list[dict]:
-    with open(file_path) as file:
-        reader = csv.DictReader(file)
+    """
+    Считывает данные из csv файла
+    :param file_path: название файла
+    :return: список транзакций
+    """
+    data_list = []
+    try:
+        with open(file_path, encoding="utf-8") as file:
+            reader = csv.DictReader(file, delimiter=";")
+            for row in reader:
+                data_list.append(row)
+            return data_list
+    except Exception as e:
+        raise e
+
 
 def excel_read(file_path: str) -> list[dict]:
-    pass
+    """
+    Считывает данные из excel файла
+    :param file_path: название файла
+    :return: список транзакций
+    """
+    data_list = []
+    try:
+        reader = pd.read_excel(file_path)
+        print(reader.shape)
+        for index, row in reader.iterrows():
+            data_list.append(dict(row))
+        return data_list
+    except Exception as e:
+        raise e
 
 
 if __name__ == "__main__":
-    pass
+    """print(csv_read("data/transactions.csv"))"""
+    print(excel_read("data/transactions_excel.xlsx"))
---
+++ tests/test_reader.py
@@ -0,0 +1,13 @@
+import pytest
+
+from src import reader
+
+from unittest.mock import patch
+
+def test_csv_reader() -> None:
+    with patch("builtins.open") as mock_open:
+        pass
+    pass
+
+def test_excel_reader() -> None:
+    pass
